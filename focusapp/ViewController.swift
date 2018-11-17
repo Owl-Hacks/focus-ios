24da1a0821ceefc26805d90d54475e1ca08a974a
Jacobvs <jacobvs2@gmail.com>
2018-11-17 14:42:38 -0800

    Initial Commit

---
+++ focusapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  focusapp
+//
+//  Created by Jacob van Steyn on 11/17/18.
+//  Copyright © 2018 Jacobvs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
